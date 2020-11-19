4dd4e15c7e35d66956d74bc4e4231b24b64aeb2c
Aaron Soto <imsoto1215@gmail.com>
2020-11-18 23:30:03 -0600

    It works correctly but after the first reload in offline mode

--- js/app.js
+++ js/app.js
@@ -1,60 +1,55 @@
-if( navigator.serviceWorker ) {
-    navigator.serviceWorker.register('/sw.js')
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/sw.js");
 }
 
-
-const wnumber      = document.getElementById('wnumber')
-const selector     = document.getElementById('selector')
-const formWhatsapp = document.getElementById('formWhatsapp')
+const wnumber = document.getElementById("wnumber");
+const selector = document.getElementById("selector");
+const formWhatsapp = document.getElementById("formWhatsapp");
 
 const headers = new Headers({
-    'Content-Type':'application/json'
-})
+  "Content-Type": "application/json",
+});
 
 const init = {
-    method: 'GET',
-    headers
-}
+  method: "GET",
+  headers,
+};
 
 fetch('https://country-cities.herokuapp.com/api/v0.1/countries/codes', init)
-.then(function(response) {
+  .then(function (response) {
     return response.json();
   })
-  .then(function(myJson) {
-    myJson.data.forEach(element => {
-        let option = document.createElement('option')
-        option.value = element.dial_code
-        option.innerHTML = element.name
-        selector.appendChild(option)
+  .then(function (myJson) {
+    myJson.data.forEach((element) => {
+      let option = document.createElement("option");
+      option.value = element.dial_code;
+      option.innerHTML = element.name;
+      selector.appendChild(option);
     });
   });
 
 // ****************
 const sendMessage = (e) => {
-    e.preventDefault()
-    const phoneNumber = wnumber.value
-    if( phoneNumber.length > 10 || phoneNumber.length < 10) {
-        alert("El numero debe ser valido")
-        return
-    }
-    const completePhoneNumber = createValidNumber(selector.value + wnumber.value)
-    // TODO is wnumber legth is bigger than 10 return and tell the user it should be just 10 digits
-    window.open(`https://wa.me/${completePhoneNumber}`)
-}
+  e.preventDefault();
+  const phoneNumber = wnumber.value;
+  if (phoneNumber.length > 10 || phoneNumber.length < 10) {
+    alert("El numero debe ser valido");
+    return;
+  }
+  const completePhoneNumber = createValidNumber(selector.value + wnumber.value);
+  // TODO is wnumber legth is bigger than 10 return and tell the user it should be just 10 digits
+  window.open(`https://wa.me/${completePhoneNumber}`);
+};
 
-// **************** 
+// ****************
 const createValidNumber = (str) => {
-    let validNumber = ""
-    for (let s of str) {
-        if( s.match(/^[0-9]$/)) 
-            validNumber += s
-    }
-    return validNumber
-}
-
+  let validNumber = "";
+  for (let s of str) {
+    if (s.match(/^[0-9]$/)) validNumber += s;
+  }
+  return validNumber;
+};
 
 //wnumber.addEventListener('input', updateValue)
 //sendButton.addEventListener('click', sendMessage)
-formWhatsapp.addEventListener('submit', sendMessage, true)
-
-
+formWhatsapp.addEventListener("submit", sendMessage, true);
--- sw.js
+++ sw.js
@@ -4,13 +4,15 @@ const dynamic_cache = "dynamic-v1";
 //TODO clear old cache and manage update ui"
 
 const app_shell = [
-  "index.html",
-  "css/style.css",
-  "img/favicon.ico",
-  "js/app.js",
+    '/',
+    'index.html',
+    'css/style.css',
+    'img/favicon.ico',
+    'js/app.js',
+    'manifest.json'
 ];
 
-self.addEventListener("install", (e) => {
+self.addEventListener('install', (e) => {
   const cache_static = caches.open(static_cache).then((cache) => {
     cache.addAll(app_shell);
   });
@@ -18,32 +20,68 @@ self.addEventListener("install", (e) => {
   e.waitUntil(cache_static);
 });
 
-self.addEventListener("fetch", (e) => {
-  const respuesta = caches.match(e.request).then((res) => {
-    if (res !== undefined) {
-      console.log("------------------------");
-      console.log(res);
-      if (res) {
-        console.log("primer if");
-        return res;
-      } else {
-        return fetch(e.request).then((newRes) => {
-          console.log("la nueva respuesta es: ", newRes);
-          if (newRes.ok) {
-            return caches.open(dynamic_cache).then((cache) => {
-              console.log("i am here");
-              cache.put(e.request, newRes.clone());
-              return newRes.clone();
-            });
-          } else {
-            return newRes;
-          }
-        });
-      }
-    } else {
-      return;
-    }
-  });
+// self.addEventListener('fetch', (e) => {
+//   const respuesta = caches.match(e.request).then((res) => {
+//     if (res !== undefined) {
+//       console.log(res);
+//       if (res) {
+//         console.log("primer if");
+//         return res;
+//       } else {
+//         return fetch(e.request).then((newRes) => {
+//           console.log("la nueva respuesta es: ", newRes);
+//           if (newRes.ok) {
+//             return caches.open(dynamic_cache).then((cache) => {
+//               console.log("i am here");
+//               cache.put(e.request, newRes.clone());
+//               return newRes.clone();
+//             });
+//           } else {
+//             return newRes;
+//           }
+//         });
+//       }
+//       e.respondWith(respuesta);
+//     } else {
+//       return;
+//     }
+//   });
+// });
 
-  e.respondWith(respuesta);
-});
+// self.addEventListener('fetch', function (event) {
+//     console.log('*****')
+//     console.log(event.request)
+//     event.respondWith(
+//         caches.open(dynamic_cache)
+//             .then(function(cache) {
+//                 cache.match(event.request)
+//                     .then( function(cacheResponse) {
+//                         if(cacheResponse)
+//                             return cacheResponse
+//                         else
+//                             return fetch(event.request)
+//                                 .then(function(networkResponse) {
+//                                     cache.put(event.request, networkResponse.clone())
+//                                     return networkResponse
+//                                 })
+//                     })
+//             })
+//     )
+// });
+
+self.addEventListener('fetch', function(e){
+    const respuesta = caches.match(e.request).then(res => {
+        if( res ) return res
+        return fetch( e.request ).then( newRes => {
+            //TODO It works correctly but after the first reload in offline mode
+            // it doesnt work and a second reload has to be done to go back to normal
+            if(newRes.ok) {
+                caches.open(dynamic_cache).then( cache => {
+                    cache.put(e.request, newRes)
+                })
+            }
+            return newRes.clone()
+        })
+    })
+    e.respondWith( respuesta )
+})
